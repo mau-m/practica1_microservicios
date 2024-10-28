d963ac5b2b49abfbde7fa9dcdcaf0e3403dd074d
Mauricio <mirandabmau@gmail.com>
2024-10-27 23:28:55 -0600

    Agregar las clases para la vista y renombrar los paquetes

--- src/main/java/com/uam/izt/mx/microservicios/Practica1_microservicios.java
+++ src/main/java/com/uam/izt/mx/microservicios/Practica1_microservicios.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package mx.com.uami.microservicios;
+package com.uam.izt.mx.microservicios;
 
 /**
  *
