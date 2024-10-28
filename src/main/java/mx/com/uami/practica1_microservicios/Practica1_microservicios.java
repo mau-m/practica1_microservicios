c34d969fc38f8ff3fa2b5e0a842f6a5d1bd1be06
Mauricio <mirandabmau@gmail.com>
2024-10-27 19:58:45 -0600

    Creacion del proyecto

---
+++ src/main/java/mx/com/uami/practica1_microservicios/Practica1_microservicios.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.com.uami.practica1_microservicios;
+
+/**
+ *
+ * @author mau
+ */
+public class Practica1_microservicios {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
