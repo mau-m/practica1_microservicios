4531101d4efc28bbd87846a9ac93d1c2ffd515bc
Mauricio <mirandabmau@gmail.com>
2024-10-27 20:29:03 -0600

    Refactorizacion

--- src/main/java/mx/com/uami/microservicios/Practica1_microservicios.java
+++ src/main/java/mx/com/uami/microservicios/Practica1_microservicios.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package mx.com.uami.practica1_microservicios;
+package mx.com.uami.microservicios;
 
 /**
  *
